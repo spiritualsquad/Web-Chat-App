477dba0b92c3b4ae2fc87c9f52298bdf74d31c78
spiritualsquad <dondikshu@gmail.com>
2024-04-30 15:52:16 -0400

    Backend Completed Moving up to frontend

--- demo/src/main/java/com/spiritualquad/chat/config/WebSocketEventListener.java
+++ demo/src/main/java/com/spiritualquad/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.spiritualquad.chat.config;
 
+import com.spiritualquad.chat.chat.ChatMessage;
+import com.spiritualquad.chat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,11 +14,21 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
-
+    private final SimpMessageSendingOperations messageTemplate;
     @EventListener
     public void handleWebSocketDisconnectListener(
         SessionDisconnectEvent event
     ){
-        //todo ---
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username =(String) headerAccessor.getSessionAttributes().get("username");
+        if(username !=null){
+            log.info("User disconnected; {}",username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public",chatMessage);
+
+        }
     }
 }
